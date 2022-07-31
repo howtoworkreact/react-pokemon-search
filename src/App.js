6e7cef387155ca08e945f8d9251240a86630a8c1
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-07-31 18:30:15 +0600

    added component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
 import pokemon from "./pokemon.json";
+
+const PokemonRow = ({ pokemon }) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
 function App() {
   return (
     <div
@@ -19,10 +27,7 @@ function App() {
         </thead>
         <tbody>
           {pokemon.slice(0, 20).map((pokemon) => (
-            <tr key={pokemon.id}>
-              <td>{pokemon.name.english}</td>
-              <td>{pokemon.type.join(", ")}</td>
-            </tr>
+            <PokemonRow pokemon={pokemon} key={pokemon.id}></PokemonRow>
           ))}
         </tbody>
       </table>
