4427b9355385c4c2bfccfb3050a65d2133767a6a
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-08-01 17:43:46 +0600

    select pokemon

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import pokemon from "./pokemon.json";
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-const PokemonRow = ({ pokemon }) => (
+const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button onClick={() => onSelect(pokemon)}>select</button>
+    </td>
   </tr>
 );
 
@@ -17,10 +20,12 @@ PokemonRow.propTypes = {
     }),
     type: PropTypes.arrayOf(PropTypes.string),
   }),
+  onSelect: PropTypes.func,
 };
 
 function App() {
   const [filter, setFilter] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
   return (
     <div
       style={{
@@ -31,24 +36,45 @@ function App() {
     >
       <h1 className="title">Pokemon Search Engine</h1>
       <input value={filter} onChange={(evt) => setFilter(evt.target.value)} />
-      <table width="100%">
-        <thead>
-          <tr>
-            <th>Home</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {pokemon
-            .filter((pokemon) =>
-              pokemon.name.english.toLowerCase().includes(filter.toLowerCase())
-            )
-            .slice(0, 20)
-            .map((pokemon) => (
-              <PokemonRow pokemon={pokemon} key={pokemon.id}></PokemonRow>
-            ))}
-        </tbody>
-      </table>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: " 70% 30%",
+          gridColumnCap: "1rem",
+        }}
+      >
+        <div>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Home</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
+                .slice(0, 20)
+                .map((pokemon) => (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={pokemon.id}
+                    onSelect={(pokemon) => setSelectedItem(pokemon)}
+                  ></PokemonRow>
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && (
+          <div>
+            <h1 style={{ color: "orange" }}>{selectedItem.name.english}</h1>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
